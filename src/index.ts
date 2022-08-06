987e88dba712cef135a26c57d89ba32628c167e3
Alex Vapniaruk <alexvap23@gmail.com>
2022-08-06 20:57:34 +0300

    Lesson 99

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-class Sorter {
-    constructor(public collection: /*TODO*/) {}
-    
-    sort(): void {
-        const { length } = this.collection;
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-        for (let i = 0; i < length; i++) {
-            for(let j = 0; j < length - i - 1; j++) {
-                if(this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j+1];
-                    this.collection[j+1] = leftHand;
-                }
-            }
-        }
-    }
-}
+const numberCollection = new NumbersCollection([10, 7, 3, -5, 0]);
+numberCollection.sort();
+console.log(numberCollection.data);
 
-const sorter = new Sorter([10, 7, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
+const charactersCollection = new CharactersCollection('GFdfsjsjfKJFDDSslfd');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
